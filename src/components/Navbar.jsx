be9557a1f1a802351e770f9159ebd4a91681948f
Mohd Zaid <[razvizaid259@gmail.com]>
2024-08-26 10:08:06 +0530

    quick fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,17 +53,17 @@ function Navbar() {
 
   return (
     <>
-      <div className='fixed top-0 z-20 w-full flex text-black justify-around items-center backdrop-blur-sm p-3 shadow-lg'>
+      <div className='fixed top-0 z-20 w-full flex text-black justify-around items-center backdrop-blur-sm py-3 md:p-3 shadow-lg'>
         <NavLink to='' className='flex cursor-pointer'>
-          <img loading='lazy' className='w-9' src={logo1} alt="" />
-          <span className='pl-2 pt-1 text-xl font-bold'>NextGen</span>
+          <img loading='lazy' className='w-8 md:w-9' src={logo1} alt="" />
+          <span className='md:pl-2 pt-1 text-lg md:text-xl font-bold'>NextGen</span>
         </NavLink>
-        <div className='flex justify-center gap-6 items-center'>
+        <div className='flex justify-center gap-3 md:gap-6 items-center'>
           <div onClick={handleHam} className="sm:hidden">
             {ham ? (
-              <img loading="lazy" className="w-7 p-1" src={close} alt="close" />
+              <img loading="lazy" className="w-6 md:w-7 p-1" src={close} alt="close" />
             ) : (
-              <img loading="lazy" className="w-7" src={menu} alt="menu" />
+              <img loading="lazy" className="w-6 md:w-7" src={menu} alt="menu" />
             )}
           </div>
           <ul onClick={handleHam} className={`absolute sm:static top-16 right-2 z-10 flex flex-col sm:flex-row sm:gap-5 gap-1 ${ham ? 'flex bg-black/70 text-white px-7 py-5 rounded-lg sm:bg-transparent sm:text-current sm:px-0 sm:py-0 sm:rounded-none' : 'hidden sm:flex'}`}>
@@ -74,9 +74,9 @@ function Navbar() {
           <div className='hidden xl:block'>
             <Search/>
           </div>
-          <div onClick={()=>{userData?navigate('/product'):loginWithRedirect()}} className={`${user?'':'hover:bg-pink-500 hover:invert pb-[6px] p-2 active:bg-pink-600 rounded-md '} flex justify-center items-center gap-2`}>
-            {user?.picture?<img className='w-8 cursor-pointer rounded-full' onClick={()=>navigate('/profile')} src={user.picture}/>:<img className='cursor-pointer w-6' src={profile}/>}
-            {user?'':<span className='text-lg font-quicksand font-semibold -mb-1'>Login</span>}
+          <div onClick={()=>{userData?navigate('/product'):loginWithRedirect()}} className={`${user?'':'hover:bg-pink-500 hover:invert pb-[6px] p-2 active:bg-pink-600 rounded-md '} flex mb-1 justify-center items-center gap-2`}>
+            {user?.picture?<img className='w-8 cursor-pointer rounded-full' onClick={()=>navigate('/profile')} src={user.picture}/>:<img className='cursor-pointer w-5 md:w-6' src={profile}/>}
+            {user?'':<span className='text-base md:text-lg font-quicksand font-semibold -mb-1'>Login</span>}
           </div>
           {visible && <AnimatePop initial={{scale:0}} animate={{scale:1}} className={`animate-bounce duration-100 absolute top-16 right-0 md:right-[95px] drop-shadow-md`}/>}
           <img onClick={()=>navigate('cart')} className='cursor-pointer  hidden md:block w-[26px]' src={cart} alt="cart" />
