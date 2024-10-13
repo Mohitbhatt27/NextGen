57b8976063a5566ebb4000b7b3bd43d9cbd8095b
Mohit <mohit.peak@gmail.com>
2024-10-13 17:16:54 +0530

    Removed unnecessary dispatch of cart data from app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
-import React,{useEffect,lazy,Suspense} from 'react'
-const Navbar=lazy(()=>import('./components/Navbar'))
-const Footer=lazy(()=>import('./components/Footer'))
-const Helpdesk=lazy(()=>import('./components/Helpdesk'))
-import { Outlet } from 'react-router-dom'
-import { useAuth0 } from '@auth0/auth0-react'
-import { useDispatch } from 'react-redux'
-import {login,logout} from './Redux/authSlice'
-import { addToCart } from './Redux/cartSlice'
+import React, { useEffect, lazy, Suspense } from "react";
+const Navbar = lazy(() => import("./components/Navbar"));
+const Footer = lazy(() => import("./components/Footer"));
+const Helpdesk = lazy(() => import("./components/Helpdesk"));
+import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./Redux/authSlice";
+import { addToCart } from "./Redux/cartSlice";
 
 function App() {
-  const {user,isAuthenticated,isLoading}=useAuth0()
-  const dispatch=useDispatch()
-  
-  useEffect(()=>{
-    let cartData=JSON.parse(localStorage.getItem('cartItems'));
-    if(cartData && cartData.length>0){
-      dispatch(addToCart(cartData));
-    }
-  },[dispatch])
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    if(!isLoading && isAuthenticated && user){
-      dispatch(login(user))
-    }else{
-      dispatch(logout())
-    }
-  },[isLoading,isAuthenticated,user])
+  // useEffect(()=>{
+  //   let cartData=JSON.parse(localStorage.getItem('cartItems'));
+  //   if(cartData && cartData.length>0){
+  //     dispatch(addToCart(cartData));
+  //   }
+  // },[dispatch])
 
-  if(isLoading) return <p>Loading...</p>
+  useEffect(() => {
+    if (!isLoading && isAuthenticated && user) {
+      dispatch(login(user));
+    } else {
+      dispatch(logout());
+    }
+  }, [isLoading, isAuthenticated, user]);
+
+  if (isLoading) return <p>Loading...</p>;
   return (
-    <Suspense fallback=''>
-      <Navbar/>
-      <Outlet/>
-      <Helpdesk/>
-      <Footer/>
+    <Suspense fallback="">
+      <Navbar />
+      <Outlet />
+      <Helpdesk />
+      <Footer />
     </Suspense>
-  )
+  );
 }
 
-export default App
+export default App;
